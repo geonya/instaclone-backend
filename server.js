1ecc080a44806f694a80b43def3b37abd42407d2
geonya <geony8410@gmail.com>
2022-04-20 13:04:22 +0900

    #3.12 clear

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
 	schema,
 });
 
-server
-	.listen(PORT)
-	.then(() => console.log(`🚀  Server ready at ${process.env.PORT} ✅`));
+server.listen(PORT).then(() => console.log(`🚀 Server ready at ${PORT} ✅`));
