005a346187a8f85f4ff7a14638a404543361683b
geonya <geony8410@gmail.com>
2022-04-21 09:44:48 +0900

    #4.5 login jwt token

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,7 @@
 import client from "../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 export default {
 	Mutation: {
 		createAccount: async (
@@ -30,6 +32,29 @@ export default {
 			} catch (e) {
 				return e;
 			}
+		},
+		login: async (_, { username, password }) => {
+			// find user with args.username
+			const user = await client.user.findFirst({ where: { username } });
+			if (!user) {
+				return {
+					ok: false,
+					error: "User not found.",
+				};
+			}
+			const passwordOk = await bcrypt.compare(password, user.password);
+			if (!passwordOk) {
+				return {
+					ok: false,
+					error: "Incorrect Password",
+				};
+			}
+			// issue a token and send it to the user
+			const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+			return {
+				ok: true,
+				token,
+			};
 		},
 	},
 };
