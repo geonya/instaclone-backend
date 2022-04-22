ff4b117dcb00de2d6b62ed297112061b4430e230
geonya <geony8410@gmail.com>
2022-04-22 12:43:15 +0900

    #4.24 offset pagination

---
+++ src/users/seeFollowers/seeFollowers.resolvers.ts
@@ -0,0 +1,32 @@
+export default {
+	Query: {
+		seeFollowers: async (_: any, { username, page }, { client }) => {
+			const ok = await client.user.findUnique({
+				where: { username },
+				select: {
+					id: true,
+				},
+			});
+			if (!ok) {
+				return {
+					ok: false,
+					error: "User doesn't exist",
+				};
+			}
+			const followers = await client.user
+				.findUnique({ where: { username } })
+				.followers({
+					take: 5,
+					skip: (page - 1) * 5,
+				});
+			const totalFollowers = await client.user.count({
+				where: { following: { some: { username } } },
+			});
+			return {
+				ok: true,
+				followers,
+				totalPages: Math.ceil(totalFollowers / 5),
+			};
+		},
+	},
+};
