42e02c80a5ae715687a97d83cb52a1ab8e54923b
geonya <geony8410@gmail.com>
2022-04-22 10:53:24 +0900

    #4.21 follow user resolver

---
+++ src/users/followUser/followUser.resolvers.ts
@@ -0,0 +1,36 @@
+import { protectedResolver } from "../users.utils";
+
+export default {
+	Mutation: {
+		followUser: protectedResolver(
+			async (_, { username }, { loggedInUser, client }) => {
+				const ok = await client.user.findUnique({
+					where: {
+						username,
+					},
+				});
+				if (!ok) {
+					return {
+						ok: false,
+						error: "that user does not exist.",
+					};
+				}
+				await client.user.update({
+					where: {
+						id: loggedInUser.id,
+					},
+					data: {
+						following: {
+							connect: {
+								username,
+							},
+						},
+					},
+				});
+				return {
+					ok: true,
+				};
+			}
+		),
+	},
+};
