d6812964fd53bfa6c76f63e652e59b7692057235
geonya <geony8410@gmail.com>
2022-04-28 16:44:31 +0900

    #6.17 Edit Comment

---
+++ src/comments/editComment/editComment.typeDefs.ts
@@ -0,0 +1,11 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+	type EditCommentResult {
+		ok: Boolean!
+		error: String
+	}
+	type Mutation {
+		editComment(id: Int!, payload: String!): EditCommentResult
+	}
+`;
