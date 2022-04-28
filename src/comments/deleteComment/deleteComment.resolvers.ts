877526b97c8eb2320f4381b97fe6a0a244811479
geonya <geony8410@gmail.com>
2022-04-28 16:33:57 +0900

    #6.16 Delete comment and photo

---
+++ src/comments/deleteComment/deleteComment.resolvers.ts
@@ -0,0 +1,30 @@
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+	Mutation: {
+		deleteComment: protectedResolver(
+			async (_: any, { id }, { client, loggedInUser }) => {
+				const comment = await client.comment.findUnique({
+					where: { id },
+					select: { userId: true },
+				});
+				if (!comment) {
+					return {
+						ok: false,
+						error: "Comment not found.",
+					};
+				} else if (comment.userId !== loggedInUser.id) {
+					return {
+						ok: false,
+						error: "Not Authorized.",
+					};
+				} else {
+					await client.comment.delete({ where: { id } });
+					return {
+						ok: true,
+					};
+				}
+			}
+		),
+	},
+};
---
+++ src/photos/deletePhoto/deletePhoto.resolvers.ts
@@ -0,0 +1,30 @@
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+	Mutation: {
+		deletePhoto: protectedResolver(
+			async (_: any, { id }, { client, loggedInUser }) => {
+				const photo = await client.photo.findUnique({
+					where: { id },
+					select: { userId: true },
+				});
+				if (!photo) {
+					return {
+						ok: false,
+						error: "Photo not found.",
+					};
+				} else if (photo.userId !== loggedInUser.id) {
+					return {
+						ok: false,
+						error: "Not Authorized",
+					};
+				} else {
+					await client.photo.delete({ where: { id } });
+					return {
+						ok: true,
+					};
+				}
+			}
+		),
+	},
+};
