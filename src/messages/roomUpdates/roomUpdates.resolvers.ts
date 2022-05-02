e00af7cca7c65cc19ae7619f7f340717ec67cbe6
geonya <geony8410@gmail.com>
2022-05-02 12:57:13 +0900

    #7.10 Filtering Subscriptions part Two

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,16 +1,26 @@
 import { withFilter } from "graphql-subscriptions";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
 	Subscription: {
 		roomUpdates: {
-			subscribe: withFilter(
-				() => pubsub.asyncIterator(NEW_MESSAGE),
-				({ roomUpdates }, { id }) => {
-					return roomUpdates.roomId === id;
+			subscribe: async (root, args, context, info) => {
+				const room = await client.room.findUnique({
+					where: { id: args.id },
+					select: { id: true },
+				});
+				if (!room) {
+					throw new Error("You shall not see this.");
 				}
-			),
+				return withFilter(
+					() => pubsub.asyncIterator(NEW_MESSAGE),
+					({ roomUpdates }, { id }) => {
+						return roomUpdates.roomId === id;
+					}
+				)(root, args, context, info);
+			},
 		},
 	},
 };
