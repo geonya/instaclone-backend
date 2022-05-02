7e3d16e5eb3a18dea57f17bf73eecf9e67091f27
geonya <geony8410@gmail.com>
2022-05-02 11:34:28 +0900

    #7.9 Filtering Subscriptions part One

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,10 +1,16 @@
+import { withFilter } from "graphql-subscriptions";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
 	Subscription: {
 		roomUpdates: {
-			subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+			subscribe: withFilter(
+				() => pubsub.asyncIterator(NEW_MESSAGE),
+				({ roomUpdates }, { id }) => {
+					return roomUpdates.roomId === id;
+				}
+			),
 		},
 	},
 };
