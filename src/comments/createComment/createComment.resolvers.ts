7ac5b6cc5def97dfb660f2ce7a261b05f4268c46
geonya <geony8410@gmail.com>
2022-05-16 11:50:15 +0900

    ch.12.3

--- src/comments/createComment/createComment.resolvers.ts
+++ src/comments/createComment/createComment.resolvers.ts
@@ -18,7 +18,7 @@ export default {
 						ok: false,
 						error: "Photo not found.",
 					};
-				await client.comment.create({
+				const newComment = await client.comment.create({
 					data: {
 						payload,
 						photo: {
@@ -35,6 +35,7 @@ export default {
 				});
 				return {
 					ok: true,
+					id: newComment.id,
 				};
 			}
 		),
--- src/shared/shared.typeDefs.ts
+++ src/shared/shared.typeDefs.ts
@@ -4,5 +4,6 @@ export default gql`
 	type MutationResponse {
 		ok: Boolean!
 		error: String
+		id: Int
 	}
 `;
